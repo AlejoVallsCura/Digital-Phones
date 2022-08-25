8977459748953502ef84b06d89fd5ae9c5d2f372
Alejo Valls Cura <alejo.valls.c@gmail.com>
2022-08-25 14:28:24 -0300

    Creando la App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          E-Commerce en proceso de desarrollo
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Digital Phones
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
