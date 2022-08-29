bb1fa4cb822fffc198d3902d48291c152923bc40
Alejo Valls Cura <alejo.valls.c@gmail.com>
2022-08-29 15:13:48 -0300

    NavBar - Alejo Valls Cura

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/NavBar/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          E-Commerce en proceso de desarrollo
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Digital Phones
-        </a>
-      </header>
+    
     </div>
+    </>
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./NavBar.css"
+import React from 'react'
+import logo from "../../Images/LogoDigital.png"
+
+const NavBar = () => {
+  return (
+    <header className="contenedor">
+        <div className="contenedorNav">
+        <img className="logo" src={logo} alt="logo" />
+        <nav>
+            <ul className="nav__links">
+                <li><a href="#">Servicio</a></li>
+                <li><a href="#">Tienda</a></li>
+                <li><a href="#">Nosotros</a></li>
+            </ul>
+        </nav>
+        <a className="cta" href="#"><button> Contactame!</button> </a>
+        </div>
+    </header>
+  )
+}
+
+export default NavBar
+
+
